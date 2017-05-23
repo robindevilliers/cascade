3e7709963fce58b597ec93a0eb289dd00213f7d0
robindevilliers <robindevilliers@me.com>
2017-05-23 14:01:19 +0100

    finished tests

---
+++ second-example-application/src/test/java/uk/co/malbec/welcometohell/steps/Booking.java
@@ -0,0 +1,54 @@
+package uk.co.malbec.welcometohell.steps;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import uk.co.malbec.cascade.annotations.Demands;
+import uk.co.malbec.cascade.annotations.Step;
+import uk.co.malbec.cascade.annotations.Then;
+import uk.co.malbec.cascade.annotations.When;
+
+import static org.junit.Assert.assertEquals;
+import static uk.co.malbec.welcometohell.Utilities.selectOption;
+import static uk.co.malbec.welcometohell.Utilities.waitForPage;
+
+@Step({ThrallResults.class, ThrallQuestion.NoToThrall.class})
+public interface Booking {
+    class YesToMotherInLaw implements Booking {
+
+        @Demands
+        private WebDriver webDriver;
+
+        @When
+        public void when() {
+            selectOption(webDriver, "input", "true");
+
+            webDriver.findElement(By.cssSelector("button[type=submit]")).click();
+            waitForPage(webDriver);
+        }
+
+        @Then
+        public void then() {
+            assertEquals("Welcome to Hell | Missed Booking", webDriver.getTitle());
+        }
+    }
+
+    class NoToMotherInLaw implements Booking {
+
+        @Demands
+        private WebDriver webDriver;
+
+        @When
+        public void when() {
+            selectOption(webDriver, "input", "false");
+
+            webDriver.findElement(By.cssSelector("button[type=submit]")).click();
+            waitForPage(webDriver);
+        }
+
+        @Then
+        public void then() {
+            assertEquals("Welcome to Hell | Missed Booking", webDriver.getTitle());
+        }
+    }
+
+}
